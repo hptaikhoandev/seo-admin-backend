b9fdcc07d09bf0734220774c70ba61a6083a4294
ejim-hp <jimm@darasa.io>
2025-03-05 11:30:19 +0800

    Add user permision for subdomain
    Add user permision for subdomain

--- controllers/subDomainController.js
+++ controllers/subDomainController.js
@@ -2,90 +2,194 @@ const { Op, Sequelize } = require('sequelize');
 const SubDomain = require("../models/subdomain");
 const AccountIds = require("../models/accountId");
 
-exports.findAllSubDomain = async (req, res) => {
-    const { page, limit, search, sortBy, sortDesc } = req.query;
-    const offset = (page - 1) * limit;
-    const whereClause = search ? {
-        [Op.or]: [
-          { name: { [Op.like]: `%${search}%` } },
-          { account_id: { [Op.like]: `%${search}%` } },
-          { content: { [Op.like]: `%${search}%` } }
-        ]
-      } : {};
-    const order = sortBy ? [
-      [sortBy, (sortDesc === 'true' ) ? 'DESC' : 'ASC']
-    ] : [];
+// exports.findAllSubDomain = async (req, res) => {
+//     const { page, limit, search, sortBy, sortDesc, team } = req.query;
+//     const offset = (page - 1) * limit;
+//     const whereClause = search ? {
+//         [Op.or]: [
+//           { name: { [Op.like]: `%${search}%` } },
+//           { account_id: { [Op.like]: `%${search}%` } },
+//           { content: { [Op.like]: `%${search}%` } }
+//         ]
+//       } : {};
+//     const order = sortBy ? [
+//       [sortBy, (sortDesc === 'true' ) ? 'DESC' : 'ASC']
+//     ] : [];
 
-    try {
-      // Step 1: Get the total distinct count (without limit/offset)
-      const totalCount = await SubDomain.count({
-        where: whereClause,
-        distinct: true, // Ensure distinct count
-        col: 'zone_id', // Count distinct zone_id combinations
-        group: ['zone_id', 'account_id', 'name'], // Group by these fields
-      });
+//     try {
+//       // Step 1: Get the total distinct count (without limit/offset)
+//       const totalCount = await SubDomain.count({
+//         where: whereClause,
+//         distinct: true, // Ensure distinct count
+//         col: 'zone_id', // Count distinct zone_id combinations
+//         group: ['zone_id', 'account_id', 'name'], // Group by these fields
+//       });
      
-      // Step 2: Fetch paginated distinct records
-      const rows = await SubDomain.findAll({
-        where: whereClause, // Apply filtering
-        attributes: [
-          [Sequelize.fn('MAX', Sequelize.col('subdomains.id')), 'id'], // Get latest record ID per group
-          'zone_id',
-          'account_id',
-          'name',
-          [Sequelize.fn('MAX', Sequelize.col('subdomains.created_on')), 'created_on'], // Get latest createdAt per group
-          [Sequelize.fn('MAX', Sequelize.col('subdomains.modified_on')), 'modified_on'], // Get latest createdAt per group
-          [Sequelize.fn('MAX', Sequelize.col('subdomains.content')), 'content'], // Get latest createdAt per group
-          [Sequelize.fn('MAX', Sequelize.col('subdomains.type')), 'type'], // Get latest createdAt per group
-          [Sequelize.fn('MAX', Sequelize.col('subdomains.createdAt')), 'createdAt'], // Get latest createdAt per group
-          [Sequelize.literal('(SELECT email FROM accountIds WHERE accountIds.account_id = subdomains.account_id LIMIT 1)'), 'email'],
-          [Sequelize.literal('(SELECT team FROM accountIds WHERE accountIds.account_id = subdomains.account_id LIMIT 1)'), 'team'],
-          [Sequelize.fn('COUNT', Sequelize.col('subdomains.id')), 'record_count'], // Count the number of records per group
-          [
-            Sequelize.literal(`
-              (
-                SELECT sub.content 
-                FROM subdomains AS sub 
-                WHERE sub.account_id = subdomains.account_id 
-                AND sub.zone_id = subdomains.zone_id 
-                AND sub.name = subdomains.name 
-                AND sub.id = (
-                  SELECT MAX(s.id) FROM subdomains s 
-                  WHERE s.account_id = subdomains.account_id 
-                  AND s.zone_id = subdomains.zone_id 
-                  AND s.name = subdomains.name
-                )
-                AND sub.content != (
-                  SELECT s.content FROM subdomains s 
-                  WHERE s.id = (
-                    SELECT MAX(s2.id) FROM subdomains s2 
-                    WHERE s2.account_id = subdomains.account_id 
-                    AND s2.zone_id = subdomains.zone_id 
-                    AND s2.name = subdomains.name
-                  )
-                )
-                ORDER BY sub.createdAt DESC 
-                LIMIT 1
-              )
-            `), 'old_content'
+//       // Step 2: Fetch paginated distinct records
+//       const rows = await SubDomain.findAll({
+//         where: whereClause, // Apply filtering
+//         attributes: [
+//           [Sequelize.fn('MAX', Sequelize.col('subdomains.id')), 'id'], // Get latest record ID per group
+//           'zone_id',
+//           'account_id',
+//           'name',
+//           [Sequelize.fn('MAX', Sequelize.col('subdomains.created_on')), 'created_on'], // Get latest createdAt per group
+//           [Sequelize.fn('MAX', Sequelize.col('subdomains.modified_on')), 'modified_on'], // Get latest createdAt per group
+//           [Sequelize.fn('MAX', Sequelize.col('subdomains.content')), 'content'], // Get latest createdAt per group
+//           [Sequelize.fn('MAX', Sequelize.col('subdomains.type')), 'type'], // Get latest createdAt per group
+//           [Sequelize.fn('MAX', Sequelize.col('subdomains.createdAt')), 'createdAt'], // Get latest createdAt per group
+//           [Sequelize.literal('(SELECT email FROM accountIds WHERE accountIds.account_id = subdomains.account_id LIMIT 1)'), 'email'],
+//           [Sequelize.literal('(SELECT team FROM accountIds WHERE accountIds.account_id = subdomains.account_id LIMIT 1)'), 'team'],
+//           [Sequelize.fn('COUNT', Sequelize.col('subdomains.id')), 'record_count'], // Count the number of records per group
+//           [
+//             Sequelize.literal(`
+//               (
+//                 SELECT sub.content 
+//                 FROM subdomains AS sub 
+//                 WHERE sub.account_id = subdomains.account_id 
+//                 AND sub.zone_id = subdomains.zone_id 
+//                 AND sub.name = subdomains.name 
+//                 AND sub.id = (
+//                   SELECT MAX(s.id) FROM subdomains s 
+//                   WHERE s.account_id = subdomains.account_id 
+//                   AND s.zone_id = subdomains.zone_id 
+//                   AND s.name = subdomains.name
+//                 )
+//                 AND sub.content != (
+//                   SELECT s.content FROM subdomains s 
+//                   WHERE s.id = (
+//                     SELECT MAX(s2.id) FROM subdomains s2 
+//                     WHERE s2.account_id = subdomains.account_id 
+//                     AND s2.zone_id = subdomains.zone_id 
+//                     AND s2.name = subdomains.name
+//                   )
+//                 )
+//                 ORDER BY sub.createdAt DESC 
+//                 LIMIT 1
+//               )
+//             `), 'old_content'
             
-          ],
-        ],
-        group: ['zone_id', 'account_id', 'name'], // Grouping to get distinct results
-        order: [
-          [Sequelize.fn('MAX', Sequelize.col('subdomains.createdAt')), 'DESC'], // Order by most recent createdAt
-        ],
-        limit: parseInt(limit), // Apply pagination
-        offset: parseInt(offset), // Apply pagination
-      });
+//           ],
+//         ],
+//         group: ['zone_id', 'account_id', 'name'], // Grouping to get distinct results
+//         order: [
+//           [Sequelize.fn('MAX', Sequelize.col('subdomains.createdAt')), 'DESC'], // Order by most recent createdAt
+//         ],
+//         limit: parseInt(limit), // Apply pagination
+//         offset: parseInt(offset), // Apply pagination
+//       });
       
-      res.json({
-          data: rows,
-          total: totalCount.length,
-          page: parseInt(page),
-          limit: parseInt(limit),
-      });
+//       res.json({
+//           data: rows,
+//           total: totalCount.length,
+//           page: parseInt(page),
+//           limit: parseInt(limit),
+//       });
+//     } catch (error) {
+//         res.status(500).json({ error: error.message });
+//     }
+// };
+
+exports.findAllSubDomain = async (req, res) => {
+    try {
+        const { page = 1, limit = 10, search, sortBy, sortDesc, team } = req.query;
+        const offset = (parseInt(page) - 1) * parseInt(limit);
+
+        // ✅ Define Search Filter
+        const whereClause = search
+            ? {
+                [Op.and]: [
+                    {
+                        [Op.or]: [
+                            { name: { [Op.like]: `%${search}%` } },
+                            { account_id: { [Op.like]: `%${search}%` } },
+                            { content: { [Op.like]: `%${search}%` } }
+                        ]
+                    }
+                ]
+            }
+            : {};
+
+        // ✅ Sorting Logic
+        const order = sortBy ? [[sortBy, sortDesc === 'true' ? 'DESC' : 'ASC']] : [['createdAt', 'DESC']];
+
+        // ✅ Step 1: Get the Total Count (Filters by `team` unless it's "seo-admin")
+        const totalCountQuery = `
+            SELECT COUNT(DISTINCT sub.zone_id, sub.account_id, sub.name) AS total
+            FROM subdomains AS sub
+            ${team && team !== "admin" ? 'INNER JOIN accountIds AS acc ON sub.account_id = acc.account_id WHERE acc.team = :team' : ''}
+        `;
+
+        const totalCountResult = await SubDomain.sequelize.query(totalCountQuery, {
+            replacements: team && team !== "admin" ? { team } : {},
+            type: Sequelize.QueryTypes.SELECT,
+        });
+
+        const totalCount = totalCountResult[0]?.total || 0;
+
+        // ✅ Step 2: Fetch Paginated Distinct Records (Filters by `team` unless it's "seo-admin")
+        const rows = await SubDomain.findAll({
+            where: whereClause,
+            attributes: [
+                [Sequelize.fn('MAX', Sequelize.col('subdomains.id')), 'id'], // Get latest record ID per group
+                'zone_id',
+                'account_id',
+                'name',
+                [Sequelize.fn('MAX', Sequelize.col('subdomains.created_on')), 'created_on'], // Get latest createdOn per group
+                [Sequelize.fn('MAX', Sequelize.col('subdomains.modified_on')), 'modified_on'], // Get latest modifiedOn per group
+                [Sequelize.fn('MAX', Sequelize.col('subdomains.content')), 'content'], // Get latest content per group
+                [Sequelize.fn('MAX', Sequelize.col('subdomains.type')), 'type'], // Get latest type per group
+                [Sequelize.fn('MAX', Sequelize.col('subdomains.createdAt')), 'createdAt'], // Get latest createdAt per group
+                [Sequelize.literal('(SELECT email FROM accountIds WHERE accountIds.account_id = subdomains.account_id LIMIT 1)'), 'email'],
+                [Sequelize.literal('(SELECT team FROM accountIds WHERE accountIds.account_id = subdomains.account_id LIMIT 1)'), 'team'],
+                [Sequelize.fn('COUNT', Sequelize.col('subdomains.id')), 'record_count'], // Count the number of records per group
+                [
+                    Sequelize.literal(`
+                        (
+                            SELECT sub.content 
+                            FROM subdomains AS sub 
+                            WHERE sub.account_id = subdomains.account_id 
+                            AND sub.zone_id = subdomains.zone_id 
+                            AND sub.name = subdomains.name 
+                            AND sub.id = (
+                                SELECT MAX(s.id) FROM subdomains s 
+                                WHERE s.account_id = subdomains.account_id 
+                                AND s.zone_id = subdomains.zone_id 
+                                AND s.name = subdomains.name
+                            )
+                            AND sub.content != (
+                                SELECT s.content FROM subdomains s 
+                                WHERE s.id = (
+                                    SELECT MAX(s2.id) FROM subdomains s2 
+                                    WHERE s2.account_id = subdomains.account_id 
+                                    AND s2.zone_id = subdomains.zone_id 
+                                    AND s2.name = subdomains.name
+                                )
+                            )
+                            ORDER BY sub.createdAt DESC 
+                            LIMIT 1
+                        )
+                    `), 'old_content'
+                ],
+            ],
+            group: ['zone_id', 'account_id', 'name'],
+            order: [[Sequelize.fn('MAX', Sequelize.col('subdomains.createdAt')), 'DESC']],
+            limit: parseInt(limit),
+            offset: parseInt(offset),
+            having: team && team !== "admin"
+                ? Sequelize.literal(`(SELECT team FROM accountIds WHERE accountIds.account_id = subdomains.account_id LIMIT 1) = '${team}'`)
+                : undefined, // Filters team inside `findAll()` unless team is "seo-admin"
+        });
+
+        res.json({
+            data: rows,
+            total: totalCount,
+            page: parseInt(page),
+            limit: parseInt(limit),
+        });
+
     } catch (error) {
+        console.error("Error in findAllSubDomain:", error);
         res.status(500).json({ error: error.message });
     }
 };
